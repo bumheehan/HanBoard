37131949f866cc5004f0fe8c8135c96e5f9eaa89
Han <kjhhjk1234@naver.com>
2019-07-14 11:06:38 +0900

    Boot 추가

---
+++ HanBoard_SpringBoot/src/main/java/xyz/bumbing/main/HanBoardApplication.java
@@ -0,0 +1,13 @@
+package xyz.bumbing.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanBoardApplication.class, args);
+	}
+
+}
